bac797fc4520a036496d2d20b6d5912e03835f33
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-04-01 22:19:08 +0700

    Create Sdk.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

---
+++ Web API/Sdk.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
